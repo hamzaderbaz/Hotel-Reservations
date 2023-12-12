07f8d6d966be05f0028ba3a8c4068a73fe5ca19b
hamzaderbaz <hamza.derbaz@gmail.com>
2023-12-13 00:13:27 +0200

    add views and static files

--- property/urls.py
+++ property/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import PropertyList, PropertyDetail
-
+from . import views
 
 app_name = 'property'
 
 
 urlpatterns = [
-    path( '', PropertyList.as_view() , name='property_list'),
-    path('<slug:slug>', PropertyDetail.as_view() , name='property_detail'),
+    path('', PropertyList.as_view() , name='property_list'),
+    # path('<slug:slug>', PropertyDetail.as_view() , name='property_detail'),
 
 ]
--- property/views.py
+++ property/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView , DetailView , CreateView
+from django.views.generic import ListView , DetailView 
 from .models import Property
 
 
 
 class PropertyList(ListView):
+    # pass
     model = property 
     #filter
 
