dca5b7ff53ec6a4fbbdffcb1a92debe3cd25f21b
hamzaderbaz <hamza.derbaz@gmail.com>
2024-01-02 23:35:11 +0200

    add blog

---
+++ blog/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+from . import api_view
+
+
+app_name = 'blog'
+
+
+urlpatterns = [
+
+    path( '',views.PostList.as_view() , name='post_list' ),
+    path('<slug:slug>',views.PostDetail.as_view() , name='post_detail'),
+    
+    path('category/<slug:slug>',views.PostsByCategory.as_view() , name='post_by_category'),
+    path('tag/<slug:slug>',views.PostsByTags.as_view() , name='post_by_tag'),
+
+
+
+    # api urls
+
+    # path('api/list' , api_view.post_list_api , name='post_list_api'),
+    # path('api/list/<int:id>' , api_view.post_detail , name='post_detail_api'),
+    # path('api/list/<str:query>' , api_view.post_search , name='post_search_api'),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.views.generic import ListView , DetailView
+from .models import Post , Category
+from taggit.models import Tag
+from django.db.models import Count, Q
 
-# Create your views here.
+
+
+class PostList(ListView):
+    model = Post
+    paginate_by = 8
+
+    def get_queryset(self):
+        name = self.request.GET.get('q','')
+        
+        object_list = Post.objects.filter(
+            Q(title__icontains=name) | 
+            Q(description__icontains=name)
+        )
+        return object_list
+    
+
+
+class PostDetail(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] =  Tag.objects.all()
+        context['categories'] = Category.objects.all().annotate(post_count=Count('post_category'))
+        context['recent_posts'] = Post.objects.all()[:3]
+        return context
+    
+
+
+class PostsByCategory(ListView):
+    model = Post
+
+    def get_queryset(self):
+        objects_list = Post.objects.filter(
+            Q(category__name__icontains=self.kwargs['slug'])
+        )
+        return objects_list
+    
+
+
+class PostsByTags(ListView):
+    model = Post
+
+    def get_queryset(self):
+        objects_list = Post.objects.filter(
+            Q(tags__name__icontains=self.kwargs['slug'])
+        )
+        return objects_list
